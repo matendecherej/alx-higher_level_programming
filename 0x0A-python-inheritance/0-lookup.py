570b63f56dc8c522b80272c783d9c2be35b422ca
Jeweldonah <jeweldonah1@gmail.com>
2023-09-12 02:07:42 +0300

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+does contain the lookup function
+"""
+
+
+def lookup(obj):
+    """returns a list of available attributes and methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+this contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    pass
